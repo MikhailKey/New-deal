19eafe21674044cc5197d9b70bf7a8f40634a15d
Mikhail <48489639+MikhailKey@users.noreply.github.com>
2019-08-22 16:05:37 +0400

    new commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,104 +1,123 @@
-//модальное окно
-let modalButton = document.querySelectorAll('.modal-info'),
-    modalWindow = document.querySelector('.modal'),
-    arrowLeave = document.querySelectorAll('.modal-arrow__close');
-
-for (let i=0; i<modalButton.length; i++) 
-    {
-        let title = document.querySelector('.modal-title'),
-            text = document.querySelector('.modal-text');
-            modalButton[i].addEventListener('click', function () 
-        {
-           title.innerHTML = this.getAttribute('data-title');
-           text.innerHTML = this.getAttribute('data-content');
-           modalWindow.classList.add('modal-active');
-        });
-    }
-
-for (let i=0; i<arrowLeave.length; i++) {
-    arrowLeave[i].addEventListener('click', function () {
-        modalWindow.classList.remove('modal-active');
-    });
+function getElement(classCSS) {
+    return document.querySelector(classCSS);
 }
 
-//модальное окно с заявкой
-let openButton = document.querySelectorAll('.dot-wrap'),
-    formWindow = document.querySelector('.service-modal'),
-    arrowLeaves = document.querySelectorAll('.service-modal-arrow__close'),
-    addFile = document.querySelector('.service-modal-footer__add-file');
-for (let i=0; i<openButton.length; i++) 
-    {    
-        let title = document.querySelector('.service-modal-title'),
-            text = document.querySelector('.service-modal-text');   
+function getElements(classCSS) {
+    return document.querySelectorAll(classCSS);
+}
+
+function showModal(...args) 
+{    
+    let modalButton = getElements(args[0]),
+        body = document.querySelector(args[1]),
+        arrowLeave = getElements(args[2]);
         
-            openButton[i].addEventListener('click', function() 
+
+    modalButton.forEach((button) => 
+    {
+        let title = getElement(args[3]),
+            text = getElement(args[4]);
+        button.addEventListener('click', function() 
+            {
+            title.innerHTML = this.getAttribute('data-title');
+            text.innerHTML = this.getAttribute('data-content');
+            body.classList.add('modal__active');
+            });
+    })  
+
+    arrowLeave.forEach(function(arrow) 
+    {
+        arrow.addEventListener('click', function() 
+        {
+            body.classList.remove('modal__active');
+        });
+    })
+}
+
+function showServiceModal(...args) 
+{
+    let openButton = getElements(args[0]),
+        body = document.querySelector(args[1]),
+        arrowLeaves = getElements(args[2]),
+        title = getElement(args[3]),
+        text = getElement(args[4]); 
+    openButton.forEach(function(button) 
+    {
+        if (!button.innerHTML.includes('Выкуп долгов')) 
+        {
+            button.addEventListener('click', function() 
             {
                 title.innerHTML = this.getAttribute('data-title');
                 text.innerHTML = this.getAttribute('data-content');
-                formWindow.classList.add('service-modal-active');
-                if (!openButton[i].innerHTML.includes('Выкуп долгов')) 
-                {
-                addFile.style.display = 'none';
-                } else {
-                addFile.style.display = 'flex';   
-                }
+                body.classList.add('service-modal__active');
             });
-    }
-//контакты
-let contactsLayout = `
-<div class="contacts">
-    <div class="contacts-item">
-        <img class="contacts-item__image" src="img/hero/contacts/phone.png" alt="">
-        <p class="contacts-item__info">+7 (495) 374–66–55</p>
-    </div>
-    <div class="contacts-item">
-        <img class="contacts-item__image" src="img/hero/contacts/phone.png" alt="">
-        <p class="contacts-item__info">+7 (8452) 74–66–55 </p>
-    </div>
-    <div class="contacts-item">
-        <img class="contacts-item__message contacts-item__image" src="img/hero/contacts/message.png" alt="">
-        <p class="contacts-item__info">info@dolgoved.ru</p>
-    </div>				
-</div>
-<div class="contacts-map">
-Здесь должна быть карта, но её почему-то нет
-<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A620846f27cf62b0a71a587efe57c41733c6e2507ba717f27b247f25c70dbbe8b&amp;width=100%25&amp;height=300&amp;lang=ru_RU&amp;scroll=true"></script>
-</div>
-`
-let contactsButton = document.querySelector('.modal-contacts');
-contactsButton.addEventListener('click', function()
-    {
-        let title = document.querySelector('.modal-title'),
-            text = document.querySelector('.modal-text'); 
-            title.innerHTML = this.getAttribute('data-title');
-            text.innerHTML = contactsLayout;
-            modalWindow.classList.add('modal-active');
+        }
     })
-//close button
-for (let i=0; i<arrowLeaves.length; i++) {
-    arrowLeaves[i].addEventListener('click', function () {
-        formWindow.classList.remove('service-modal-active');
+
+    arrowLeaves.forEach(function(arrow) 
+    {
+        arrow.addEventListener('click', function() 
+        {
+            body.classList.remove('service-modal__active');
+        })
     })
 }
+//контакты
+function showContacts(...args) 
+{
+    let contactsButton = getElement(args[0]),
+        body = document.querySelector(args[1]),
+        closeButton = getElements(args[2]);
+    contactsButton.addEventListener('click', function() 
+    {
+        body.classList.add('modal-contacts-window__active');
+    });
+    closeButton.forEach(function(arrow) 
+    {
+        arrow.addEventListener('click', function() 
+        {
+            body.classList.remove('modal-contacts-window__active');
+        })
+    })
+}
+function showServiceDebt(...args)
+{
+    let debtButton = getElement(args[0]),
+        body = document.querySelector(args[1]),
+        closeButton = getElements(args[2]);
+    debtButton.addEventListener('click', function() 
+    {
+        body.classList.add('service-modal-debt__active');
+    });
+    closeButton.forEach(function(arrow) {
+        arrow.addEventListener('click', function() 
+        {
+            body.classList.remove('service-modal-debt__active');
+        })
+    })
+}
+function showPopup(...args) 
+{
+    let popupButton = getElement(args[0]),
+    body = document.querySelector(args[1]),
+    closeButton = getElement(args[2]);
 
-//обратная связь
-let popupButton = document.querySelector('.callback'),
-    popupWindow = document.querySelector('.popup-callback'),
-    closeButton = document.querySelector('.popup-callback-close');
-
-popupButton.addEventListener('click', function () {
-    popupWindow.style.transform = 'translateX(0%)';
-})
-closeButton.addEventListener('click', function () {
-    popupWindow.style.transform = 'translateX(100%)';
-})
-
-//добавление файла
-let inputs = document.querySelectorAll('.service-modal-footer__add-file input');
-Array.prototype.forEach.call(inputs, function(input){
-  let label	 = input.previousElementSibling,
-      labelVal = label.innerHTML;
-  input.addEventListener('change', function(e){
+    popupButton.addEventListener('click', function () 
+    {
+        body.classList.add('popup-callback__active');
+    })
+    closeButton.addEventListener('click', function () 
+    {
+        body.classList.remove('popup-callback__active');
+    })
+}
+function attachFile(...args) 
+{
+    let inputs = document.querySelectorAll(args[0]);
+    Array.prototype.forEach.call(inputs, function(input){
+    let label	 = input.previousElementSibling,
+        labelVal = label.innerHTML;
+    input.addEventListener('change', function(e){
     let fileName = '';
     if( this.files && this.files.length > 1 )
       fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
@@ -110,4 +129,14 @@ Array.prototype.forEach.call(inputs, function(input){
       label.innerHTML = labelVal;
 	});
 });
+}
+
+
+showModal('.modal-info', 'body', '.modal-arrow__close', '.modal-title', '.modal-arrow__close');
+showServiceModal('.dot-wrap', 'body ', '.service-modal-arrow__close', '.service-modal-title', '.service-modal-text');
+showContacts('.modal-contacts', 'body', '.modal-contacts__close');
+showServiceDebt('.dot-debt', 'body', '.service-debt-arrow__close');
+showPopup('.callback', 'body', '.popup-callback-close');
+attachFile('.service-modal-footer__add-file input');
+
 
